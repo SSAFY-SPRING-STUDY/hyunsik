d45a77509ed121d085220c6e87066957c0ccc33b
hyunsik <hyunsik@naver.com>
2026-03-26 13:04:38 +0900

    first commit

---
+++ src/main/java/com/example/practice/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.practice;
+
+import com.example.practice.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        String str = helloService.getString();
+
+        return str;
+    }
+}
---
+++ src/main/java/com/example/practice/HelloService.java
@@ -0,0 +1,19 @@
+package com.example.practice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+
+    public String getString() {
+        return "hello String";
+    }
+
+    // 이 파일은 서비스다! 라고 알려준다.
+    // cmd + e : 파일 빨리 찾기
+
+
+
+
+}
